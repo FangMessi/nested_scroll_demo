812d4b396c2fb02405b00c8d2ea44862d79ea8e3
Kevin Fang <kaiwen.fang@haiziwang.com>
2020-10-24 13:53:04 +0800

    init

--- app/src/main/java/com/fang/scroll/MainAdapter.kt
+++ app/src/main/java/com/fang/scroll/MainAdapter.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
